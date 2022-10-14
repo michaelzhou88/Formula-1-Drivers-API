0078302c7890e1b028a5ec0647600aed9c93dac4
Michael Zhou <zhoumichael8@gmail.com>
2022-10-14 20:32:36 +0100

    Delete functionality and email + id validation

---
+++ src/main/java/com/example/F1drivers/driver/exception/DriverNotFoundException.java
@@ -0,0 +1,12 @@
+package com.example.F1drivers.driver.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class DriverNotFoundException extends RuntimeException{
+
+    public DriverNotFoundException(String msg) {
+        super(msg);
+    }
+}
